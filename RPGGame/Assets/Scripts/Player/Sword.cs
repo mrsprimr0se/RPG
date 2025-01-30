ec9c58753ec6297fed3845f88d7c0fc6b6ce6ad6
Paulina Borszewska <borszewskapaulina@gmail.com>
2025-01-30 21:29:36 +0100

    Sword Collision, Enemy Health

--- RPGGame/Assets/Scripts/Player/Sword.cs
+++ RPGGame/Assets/Scripts/Player/Sword.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class Sword : MonoBehaviour
 {
     [SerializeField] private GameObject slashAnimationPrefab;
-    [SerializeField] private Transform slashAnimationSpawnPoint; 
+    [SerializeField] private Transform slashAnimationSpawnPoint;
+    [SerializeField] private Transform weaponCollider;
 
     private PlayerControls playerControls;
     private Animator myAnimator;
@@ -41,12 +42,18 @@ public class Sword : MonoBehaviour
     private void Attack()
     {
         myAnimator.SetTrigger("Attack");
+        weaponCollider.gameObject.SetActive(true);  
 
        slashAnimation =  Instantiate(slashAnimationPrefab, slashAnimationSpawnPoint.position, Quaternion.identity);
         slashAnimation.transform.parent = this.transform.parent;
     }
 
-    public void SwingUpFlipAnimation()
+    public void DoneAttackingAnimationEvent()
+    {
+        weaponCollider.gameObject.SetActive(false);
+    }
+
+    public void SwingUpFlipAnimationEvent()
     {
         slashAnimation.gameObject.transform.rotation = Quaternion.Euler(-180, 0, 0);
 
@@ -56,7 +63,7 @@ public class Sword : MonoBehaviour
         }
     }
 
-    public void SwingDownFlipAnimation()
+    public void SwingDownFlipAnimationEvent()
     {
         slashAnimation.gameObject.transform.rotation = Quaternion.Euler(0, 0, 0);
 
@@ -76,10 +83,12 @@ public class Sword : MonoBehaviour
         if (mousePos.x < playerScreenPoint.x) // naprzemienne przenoszenie mieczu wraz z myszka prawo, lewo
         {
             activeWeapon.transform.rotation = Quaternion.Euler(0, -180, angle);
+            weaponCollider.transform.rotation = Quaternion.Euler(0, -180, 0);
         }
         else
         {
             activeWeapon.transform.rotation = Quaternion.Euler(0, 0, angle);
+            weaponCollider.transform.rotation = Quaternion.Euler(0, 0, 0);
         }
     }
 }
