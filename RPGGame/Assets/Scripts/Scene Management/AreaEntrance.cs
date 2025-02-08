b9ffbe89c5528d1222f22ab3331247810555eeef
Paulina Borszewska <borszewskapaulina@gmail.com>
2025-02-08 19:24:44 +0100

    Camera Controller, Fade Transition, Portal Visual Effect

--- RPGGame/Assets/Scripts/Scene Management/AreaEntrance.cs
+++ RPGGame/Assets/Scripts/Scene Management/AreaEntrance.cs
@@ -11,6 +11,9 @@ public class AreaEntrance : MonoBehaviour
         if (transitionName == SceneManagement.Instance.SceneTransitionName)
         {
             PlayerController.Instance.transform.position = this.transform.position;
+            CameraController.Instance.SetPlayerCameraFollow();
+
+            UIFade.Instance.FadeToClear();  
         }
     }
 }
